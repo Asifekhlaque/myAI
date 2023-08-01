3eb0e129eaa8584a40a661d3a1b54c3a13184dfc
Asif Ekhlaque <132199879+Asifekhlaque@users.noreply.github.com>
2023-08-01 07:31:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const button = document.querySelector("button");
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.onstart = function(){
+    console.log("Speech Recognition started!")
+}
+recognition.onresult = function(event){
+    console.log(event);
+
+    const spokenwords = event.results[0][0].transcript;
+
+    console.log("spoken words are",spokenwords);
+    computerSpeech(spokenwords);
+};
+
+function computerSpeech(words){
+const speech = new SpeechSynthesisUtterance();
+speech.lang = "en-US";
+speech.pitch = 0.4;
+speech.volume = 1;
+speech.rate = 1;
+
+speech.text = words;
+ window.speechSynthesis.speak(speech);
+}
+button.addEventListener("click",() => {
+ recognition.start();
+});
